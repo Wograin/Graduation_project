3aa716a9701df7e4ef815782c8a5acfcfb9cf44d
Wograin <enkilart@mail.ru>
2019-05-27 22:06:01 +0300

    Картинки при клике

--- js/script.js
+++ js/script.js
@@ -167,30 +167,34 @@ window.addEventListener("DOMContentLoaded", () => {
 
     //------------------Pictures-----------------------------------------------------------------------------
 
-    let images = document.querySelectorAll(".works img.pa"),
-        overlay = document.querySelector(".overlay");
+    function callModalImages() {
+        let images = document.querySelectorAll(".works img.pa"),
+            overlay = document.querySelector(".overlay");
 
-    for (let i = 0; i < images.length; i++) {
-        let actual = images[i],
-            div = document.createElement("div"),
-            img = document.createElement("img");
-        img.setAttribute("src", images[i].getAttribute("src"));
+        for (let i = 0; i < images.length; i++) {
+            let actual = images[i],
+                div = document.createElement("div"),
+                img = document.createElement("img");
+            img.setAttribute("src", images[i].getAttribute("src"));
 
-        actual.addEventListener("click", (event) => {
-            event.preventDefault();
-            overlay.style.display = "block";
-            overlay.appendChild(div).appendChild(img);
-            img.classList.add("myimage");
-            document.body.style.overflow = "hidden";
+            actual.addEventListener("click", (event) => {
+                event.preventDefault();
+                overlay.style.display = "block";
+                overlay.appendChild(div).appendChild(img);
+                img.classList.add("myimage");
+                document.body.style.overflow = "hidden";
+            });
+        }
+
+        window.addEventListener("click", (event) => {
+            if (event.target === overlay) {
+                overlay.style.display = "none";
+                document.body.style.overflow = "";
+            }
         });
     }
 
-    window.addEventListener("click", (event) => {
-        if (event.target === overlay) {
-            overlay.style.display = "none";
-            document.body.style.overflow = "";
-        }
-    });
+    callModalImages();
 
     //------------------FORMA-Modal-popup-----------------------------------------------------------------------------
 
